cb4477fcd3cf6ffd50cdcec6fbbd331c5cb3cb08
huangwei <huangwei@fanggeek.com>
2019-08-23 20:27:04 +0800

    删除用不到的方法

--- http.go
+++ http.go
@@ -63,27 +63,6 @@ func GetTransport() http.RoundTripper {
     }
 }
 
-// func (t *MyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-//     return t.RoundTrip(req)
-
-//     // t.
-// }
-
-// func  MyDirector(req *http.Request) {
-// 	req.URL.Scheme = target.Scheme
-// 	req.URL.Host = target.Host
-// 	req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
-// 	if targetQuery == "" || req.URL.RawQuery == "" {
-// 		req.URL.RawQuery = targetQuery + req.URL.RawQuery
-// 	} else {
-// 		req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
-// 	}
-// 	if _, ok := req.Header["User-Agent"]; !ok {
-// 		// explicitly disable User-Agent so it's not set to default value
-// 		req.Header.Set("User-Agent", "")
-// 	}
-// }
-
 // MyReverseProxy 我的反向代理，主要是定制了 director的 scheme，其它代码都是照抄
 func MyReverseProxy(target *url.URL) *httputil.ReverseProxy {
     targetQuery := target.RawQuery
